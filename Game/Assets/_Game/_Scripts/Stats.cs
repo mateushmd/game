780a1077f9feaf9e302a4a3eb91273f1a8653cde
washingtonwagner <104013233+washingtonwagner@users.noreply.github.com>
2024-01-05 11:16:12 -0300

    Ajustes #3

--- Game/Assets/_Game/_Scripts/Stats.cs
+++ Game/Assets/_Game/_Scripts/Stats.cs
@@ -5,20 +5,56 @@ using UnityEngine;
 public class Stats : MonoBehaviour
 {
     [Header("Attributes")]
-    [SerializeField] private float maxHP;
-    [SerializeField] private float maxMP;
-    [SerializeField] private int inteligence;
-    [SerializeField] private int force;
-    [SerializeField] private int dexterity;
-    [SerializeField] private int agility;
+    [SerializeField] private float maxHP; //statID 1
+    [SerializeField] private float maxMP; //statID 2
+    [SerializeField] private int inteligence; //statID 3
+    [SerializeField] private int force; //statID 4
+    [SerializeField] private int dexterity; //statID 5
+    [SerializeField] private int agility; //statID 6
 
     [Header("(De)buffs")] 
     private List<State> buffs = new List<State>();
+    
+    public void buffMaxHP(string name, int value,  float time)
+    {
+        if(time != 0)
+            buffs.Add(new State(1, name, value, time));
+        maxHP += value;
+    }
+    
+    public void buffMaxMP(string name, int value,  float time)
+    {
+        if(time != 0)
+            buffs.Add(new State(2, name, value, time));
+        maxMP += value;
+    }
+    
+    public void buffInteligence(string name, int value,  float time)
+    {
+        if(time != 0)
+            buffs.Add(new State(3, name, value, time));
+        inteligence += value;
+    }
 
     public void buffForce(string name, int value, float time)
     {
-        buffs.Add(new State(4, name, value, time));
+        if(time != 0)
+            buffs.Add(new State(4, name, value, time));
         force += value;
+    }
+    
+    public void buffDexterity(string name, int value,  float time)
+    {
+        if(time != 0)
+            buffs.Add(new State(5, name, value, time));
+        dexterity += value;
+    }
+    
+    public void buffAgility(string name, int value,  float time)
+    {
+        if(time != 0)
+            buffs.Add(new State(6, name, value, time));
+        agility += value;
     }
     
     // Start is called before the first frame update
@@ -64,6 +100,35 @@ public class Stats : MonoBehaviour
                 agility -= value;
                 break;
         }
+    }
+
+    public List<State> getAllStates()
+    {
+        return buffs;
+    }
+
+    public List<State> getBuff()
+    {
+        List<State> st = new List<State>();
+        foreach (State str in buffs)
+        {
+            if(str.value > 0)
+                st.Add(str);
+        }
+
+        return st;
+    }
+    
+    public List<State> getDebuff()
+    {
+        List<State> st = new List<State>();
+        foreach (State str in buffs)
+        {
+            if(str.value < 0)
+                st.Add(str);
+        }
+
+        return st;
     }
 }
 
