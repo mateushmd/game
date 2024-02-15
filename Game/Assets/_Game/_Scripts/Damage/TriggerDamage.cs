fae060cefd72e28576e6b45f03cd5484f6d5e3c1
washingtonwagner <104013233+washingtonwagner@users.noreply.github.com>
2024-02-15 18:01:56 -0300

    Dano baseado em stats #4
    Possivelmente será modificado

--- Game/Assets/_Game/_Scripts/Damage/TriggerDamage.cs
+++ Game/Assets/_Game/_Scripts/Damage/TriggerDamage.cs
@@ -1,17 +1,36 @@
+using System;
+using _Game._Scripts.Utilities;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
+using Stats = _Game._Scripts.Utilities.Stats;
 
 namespace _Game._Scripts.Damage
 {
     public class TriggerDamage : MonoBehaviour
     {
-        [SerializeField] private float damage = 800;
-    
+        private float damageOnForce = 150;
+        private float damageOnInt = 20;
+        private float totalDamage = 0;
+        private Stats stats;
+        
+        private void Awake()
+        {
+            stats = GetComponentInParent<Stats>();
+        }
+
+        private void Update()
+        {
+            damageOnForce += stats.getForce();
+            damageOnInt += stats.getInteligence();
+            totalDamage = damageOnInt + damageOnForce;
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             Damageable damageable = collision.GetComponent<Damageable>();
             if(damageable != null)
             {
-                damageable.TakeDamage(damage);
+                damageable.TakeDamage(totalDamage);
             }
         }
     }
